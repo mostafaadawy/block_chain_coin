02f857a8c4e0a88c1f2802ee82d144c2bc9f8207
mostafaadawy <mostafa_adawy@ymail.com>
2023-01-21 01:01:02 +0200

    adding more trans to get the balance not 0

--- main.js
+++ main.js
@@ -91,3 +91,11 @@ theCoin.createTransaction(new Transaction('address2','address1',50))
 console.log('\n starting the miner...')
 theCoin.minePendingTransactions('adawy-address')
 console.log('\n Balance of adawy is ',theCoin.getBalanceOfAnAddress('adawy-address'))
+
+console.log('\n starting the miner again ...')
+theCoin.minePendingTransactions('adawy-address')
+console.log('\n Balance of adawy is ',theCoin.getBalanceOfAnAddress('adawy-address'))
+
+console.log('\n starting the miner again...')
+theCoin.minePendingTransactions('adawy-address')
+console.log('\n Balance of adawy is ',theCoin.getBalanceOfAnAddress('adawy-address'))
